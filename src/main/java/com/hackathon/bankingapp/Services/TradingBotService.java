ea5a13ed80652ca469224b1221d87119e3036596
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-10-27 18:18:57 -0300

    fix: create bot and fix errors

--- src/main/java/com/hackathon/bankingapp/Services/TradingBotService.java
+++ src/main/java/com/hackathon/bankingapp/Services/TradingBotService.java
@@ -7,13 +7,17 @@ import org.springframework.stereotype.Service;
 import com.hackathon.bankingapp.DTO.TradingBotRequestDTO;
 import com.hackathon.bankingapp.Entities.Asset;
 import com.hackathon.bankingapp.Entities.TradingBot;
+import com.hackathon.bankingapp.Entities.Transaction;
 import com.hackathon.bankingapp.Entities.User;
 import com.hackathon.bankingapp.Exceptions.ForbiddenException;
 import com.hackathon.bankingapp.Exceptions.NotFoundException;
 import com.hackathon.bankingapp.Repositories.TradingBotRepository;
 import com.hackathon.bankingapp.Repositories.UserRepository;
+import com.hackathon.bankingapp.Repositories.AssetRepository;
+import com.hackathon.bankingapp.Repositories.TransactionRepository;
 import com.hackathon.bankingapp.Security.JwtTokenProvider;
 import com.hackathon.bankingapp.Utils.BotConst;
+import com.hackathon.bankingapp.Utils.Enums.TransactionType;
 
 import java.util.List;
 import java.util.Map;
@@ -29,6 +33,10 @@ public class TradingBotService {
     private AccountService accountService;
     @Autowired
     private JwtTokenProvider jwtProvider;
+    @Autowired
+    private AssetRepository assetRepository;
+    @Autowired
+    private TransactionRepository transactionRepository;
 
     public void createTradingBot(TradingBotRequestDTO tradingBotRequestDTO) {
         String username = jwtProvider.getCurrentUserDetails().getUsername();
@@ -39,10 +47,11 @@ public class TradingBotService {
             throw new ForbiddenException("Invalid PIN");
         }
 
-        TradingBot tradingBot = new TradingBot();
-        tradingBot.setUser(user);
-        tradingBot.setActive(true);
-        tradingBotRepository.save(tradingBot);
+        TradingBot bot = new TradingBot();
+        bot.setUser(user);
+        bot.setActive(true);
+        bot.setLastCheckTime(System.currentTimeMillis());
+        tradingBotRepository.save(bot);
     }
 
     @Scheduled(fixedRate = 30000)
@@ -51,18 +60,73 @@ public class TradingBotService {
         Map<String, Double> currentPrices = accountService.getAssetPrices();
 
         for (TradingBot bot : activeBots) {
-            Map<String, Double> userAssets = accountService.getUserAssets();
-            for (Map.Entry<String, Double> asset : userAssets.entrySet()) {
-                List<Asset> assetHistory = accountService.getUserAssetHistory(bot.getUser(), asset.getKey());
-                double avgPurchasePrice = accountService.calculateAveragePurchasePrice(assetHistory);
-                double currentPrice = currentPrices.get(asset.getKey());
+            User user = bot.getUser();
+            List<Asset> userAssets = assetRepository.findByUser(user);
+
+            for (Asset asset : userAssets) {
+                double avgPurchasePrice = calculateAveragePurchasePrice(
+                        assetRepository.findByUserAndAssetSymbol(user, asset.getAssetSymbol()));
+                double currentPrice = currentPrices.get(asset.getAssetSymbol());
 
                 if (currentPrice <= avgPurchasePrice * BotConst.BUY_THRESHOLD) {
-                    accountService.tryToBuy(bot.getUser(), asset.getKey(), 100.0);
+                    double amountToInvest = 100.0;
+                    if (user.getBalance() >= amountToInvest) {
+                        double quantity = Math.round((amountToInvest / currentPrice) * 1e10) / 1e10;
+
+                        Asset newAsset = new Asset();
+                        newAsset.setAssetSymbol(asset.getAssetSymbol());
+                        newAsset.setQuantity(quantity);
+                        newAsset.setPurchasePrice(currentPrice);
+                        newAsset.setPurchaseDate(System.currentTimeMillis());
+                        newAsset.setUser(user);
+                        assetRepository.save(newAsset);
+
+                        user.setBalance(user.getBalance() - amountToInvest);
+                        userRepository.save(user);
+
+                        saveTransaction(user, amountToInvest, TransactionType.ASSET_PURCHASE);
+                    }
                 } else if (currentPrice >= avgPurchasePrice * BotConst.SELL_THRESHOLD) {
-                    accountService.tryToSell(bot.getUser(), asset.getKey(), asset.getValue() * 0.1);
+                    double quantityToSell = asset.getQuantity() * 0.1;
+                    double saleAmount = quantityToSell * currentPrice;
+
+                    asset.setQuantity(asset.getQuantity() - quantityToSell);
+                    if (asset.getQuantity() > 0) {
+                        assetRepository.save(asset);
+                    } else {
+                        assetRepository.delete(asset);
+                    }
+
+                    user.setBalance(user.getBalance() + saleAmount);
+                    userRepository.save(user);
+
+                    saveTransaction(user, saleAmount, TransactionType.ASSET_SELL);
                 }
             }
         }
+    }
+
+    private double calculateAveragePurchasePrice(List<Asset> assets) {
+        if (assets.isEmpty())
+            return 0;
+
+        double totalCost = assets.stream()
+                .mapToDouble(asset -> asset.getQuantity() * asset.getPurchasePrice())
+                .sum();
+
+        double totalQuantity = assets.stream()
+                .mapToDouble(Asset::getQuantity)
+                .sum();
+
+        return totalCost / totalQuantity;
+    }
+
+    private void saveTransaction(User user, double amount, TransactionType type) {
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        transaction.setTransactionType(type);
+        transaction.setTransactionDate(System.currentTimeMillis());
+        transaction.setSourceAccount(user);
+        transactionRepository.save(transaction);
     }
 }
