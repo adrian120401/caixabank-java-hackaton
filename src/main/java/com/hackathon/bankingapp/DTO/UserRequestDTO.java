a80809d00718ef1ce7736d9830871473cd1c2362
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-10-28 01:21:49 -0300

    fix: fix error message

--- src/main/java/com/hackathon/bankingapp/DTO/UserRequestDTO.java
+++ src/main/java/com/hackathon/bankingapp/DTO/UserRequestDTO.java
@@ -14,12 +14,12 @@ public class UserRequestDTO {
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters long")
     @Size(max = 127, message = "Password must be less than 128 characters long")
+    @Pattern(regexp = "^\\S*$", message = "Password cannot contain whitespace")
     @Pattern(regexp = ".*[A-Z].*", message = "Password must contain at least one uppercase letter")
     @Pattern(regexp = ".*[0-9].*", message = "Password must contain at least one digit and one special character")
     @Pattern(regexp = ".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?].*", message = "Password must contain at least one special character")
-    @Pattern(regexp = "^\\S*$", message = "Password cannot contain whitespace")
     private String password;
-
+    
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email: ${validatedValue}")
     private String email;
